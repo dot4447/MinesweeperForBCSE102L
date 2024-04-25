9aae07251c4282c79052b5b402c18af1d316ffef
Dot <adityas1709@gmail.com>
2024-04-25 21:26:59 +0530

    Added Board functionality, yet to be tested
    Initial code upload for board setup and board functions. Might need editing when testing with Play class.

---
+++ computeractions.h
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+extern int side;
+extern int numofmines;
+extern int moves;
+
+class Play;
+bool checkValidity(int row, int col);
+
+void printtest() {
+    cout << "test";
+}
+
+class Board { // All Board Settings
+    public:
+        char **board; // Creating a 2D char board
+        Board() {
+            board = new char* [side+1];
+            for (int i=0;i<=side;i++) {
+                board[i] = new char[side + 1];
+                for (int j = 0; j <= side; j++) {
+                    board[i][j] = '~';
+                }
+            }
+        }
+
+        bool checkMine(int row, int column) {     //Checks if a point is a mine or not
+            if (board[row][column] == '*')
+                return true;
+            else
+                return false;
+        }
+
+        void getMove(int *x, int *y) {      //Prompts user for input
+            cout << "Enter move coordinates: ";
+            cin >> x >> y;
+        }
+
+        int Adjacent(int row, int col, int MineArray[][2]) {        //Counts number of adjacent MineArray to display on board
+            int count = 0;
+            int adjx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int adjy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+            for (int i = 0; d < 8; d++) {
+                int pointx = row + adjx[i];
+                int pointy = col + adjy[i];
+                if (isValid(pointx, pointy)) {
+                    if (isMine(pointx, pointy))
+                        count++;        //Increase number of adjacent MineArray for every adjacent VALID cell which has a mine
+                }
+            }
+            return count;
+        }
+
+        void display() {        //Prints board
+            int i, j;
+            cout<<"    ";
+            for (i=0;i<side;i++)
+                cout<<i;
+            cout<<"\n\n";
+            for (i=0;i<side;i++) {
+                cout<<i<<"   ";
+                for (j=0;j<side;j++)
+                    cout<<board[i][j]<<"\n";
+            }
+        }
+
+        void setMines(int MineArray[][2]) {     //Initialises the Board
+            bool Locations[side*side];
+            memset(Locations, false, sizeof(Locations));
+            for (int i=0;i<MineArray;i++) {
+                int random = rand() % (side * side);
+                int x = random / side;
+                int y = random % side;
+                if (!Locations[random]) {
+                    MineArray[i][0] = x;
+                    MineArray[i][1] = y;
+                    board[MineArray[i][0]][MineArray[i][1]] = '*';
+                    Locations[random] = true;
+                }
+                else {
+                    i--;
+                }
+            }
+        }
+};
