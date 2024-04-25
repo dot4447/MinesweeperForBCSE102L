c7b62fb360533c5dee475d169fb3a2d685f34308
Dot <adityas1709@gmail.com>
2024-04-25 21:29:02 +0530

    Made main file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include "computeractions.h"
+#include "playeractions.h"
+using namespace std;
 
+int side = 9;      //Standard Easy Difficulty
+int numofmines = 10;        //It is recommended to only use standard difficulty values (ie mine to side ratios) if changing these values.
+int moves = (side * side) - numofmines;
+
+bool checkValidity(int row, int column) {
+    return ((row >= 0) && (row < side) && (column >= 0) && (column < side));
+}
+
+int main() {
+    Board User, Actual;
+    Play* Game = new Play();
+    Game->StartGame(User, Actual);
+    return 0;
+}
